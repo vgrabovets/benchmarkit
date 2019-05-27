e7eb1ff998116ff05d104b59dec716b5a826d98c
Vitaliy Grabovets <v.grabovets@gmail.com>
2019-05-27 21:43:56 +0300

    remove hardcoded path to tmp directory

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,11 +1,11 @@
 import gc
+import tempfile
 from time import sleep
 
 import numpy as np
 import pandas as pd
 import pytest
 from pandas.testing import assert_series_equal
-from path import Path
 
 from benchmarkit import benchmark, benchmark_analyze, benchmark_run
 from benchmarkit.core import benchmark_run_file
@@ -38,21 +38,23 @@ def function_without_decorator():
 
 
 def test_benchmark_run():
-    result = benchmark_run(
-        function1,
-        save_file=Path('/tmp/test_results/test_benchmark_run1.jsonl'),
-        comment='test run',
-    )
+    with tempfile.TemporaryDirectory() as temp_dir:
+        result = benchmark_run(
+            function1,
+            save_file=temp_dir + '/test_benchmark_run1.jsonl',
+            comment='test run',
+        )
     assert result[0]['name'] == 'function1'
     assert round(result[0]['best_time'], -1) == 500
     assert round(result[0]['mean_time'], -1) == 500
     assert result[0]['comment'] == 'test run'
 
     gc.disable()
-    result = benchmark_run(
-        [function1, function2, function3, function_without_decorator],
-        save_file='/tmp/test_results/test_benchmark_run2.jsonl',
-    )
+    with tempfile.TemporaryDirectory() as temp_dir:
+        result = benchmark_run(
+            [function1, function2, function3, function_without_decorator],
+            save_file=temp_dir + '/test_benchmark_run2.jsonl',
+        )
     assert len(result) == 3
 
     assert result[0]['name'] == 'function1'
@@ -73,36 +75,40 @@ def test_benchmark_run():
             sleep(0.5)
             return
 
-    result = benchmark_run(
-        function_without_decorator,
-        save_file='/tmp/test_results/test_benchmark_run3.jsonl',
-    )
+    with tempfile.TemporaryDirectory() as temp_dir:
+        result = benchmark_run(
+            function_without_decorator,
+            save_file=temp_dir + '/test_benchmark_run3.jsonl',
+        )
     assert result is None
 
     with pytest.raises(AssertionError):
-        benchmark_run(
-            function1,
-            save_file=Path('/tmp/test_results/test_benchmark_run1.json'),
-        )
+        with tempfile.TemporaryDirectory() as temp_dir:
+            benchmark_run(
+                function1,
+                save_file=temp_dir + '/test_benchmark_run1.json',
+            )
 
 
 def test_benchmark_run_file():
-    result = benchmark_run_file(
-        DATA,
-        '/tmp/test_results2',
-        comment='run benchmark file',
-    )
+    with tempfile.TemporaryDirectory() as temp_dir:
+        result = benchmark_run_file(
+            DATA,
+            temp_dir + '/test_results2',
+            comment='run benchmark file',
+        )
     assert result[0][0]['name'] == 'search_in_list'
     assert result[0][1]['name'] == 'search_in_set'
     assert result[0][0]['num_items'] == result[0][1]['num_items'] == 1000000
     assert result[0][0]['comment'] == result[0][1]['comment'] == 'run benchmark file'  # noqa
     assert result[0][0]['_id'] == result[0][1]['_id']
 
-    result = benchmark_run_file(
-        NO_BENCHMARK_FUNCTIONS_FILE,
-        '/tmp/test_results2',
-        comment='run file with no benchmark functions',
-    )
+    with tempfile.TemporaryDirectory() as temp_dir:
+        result = benchmark_run_file(
+            NO_BENCHMARK_FUNCTIONS_FILE,
+            temp_dir + '/test_results2',
+            comment='run file with no benchmark functions',
+        )
     assert result == []
 
 
